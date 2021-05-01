432500483b80250482364b487a2ca619f92410f9
Tim Shamilov <shamilovtim@gmail.com>
2021-05-01 13:36:06 -0400

    only change fill if one is passed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import * as React from "react";
-import * as ReactNativeWeb from "react-native-web";
-import PropTypes from "prop-types";
+import * as React from 'react';
+import * as ReactNativeWeb from 'react-native-web';
+import PropTypes from 'prop-types';
 
-const createReactElement =
-  ReactNativeWeb.unstable_createElement || React.createElement;
+const createReactElement = ReactNativeWeb.unstable_createElement || React.createElement;
 
 function createElement(name, type) {
   class CreateElement extends React.Component {
@@ -15,111 +14,116 @@ function createElement(name, type) {
   CreateElement.displayName = name;
 
   CreateElement.propTypes = {
-    children: PropTypes.node,
+    children: PropTypes.node
   };
 
   CreateElement.defaultProps = {
-    children: undefined,
+    children: undefined
   };
 
   return CreateElement;
 }
 
-export const Svg = createElement("Svg", "svg");
+export const Svg = createElement('Svg', 'svg');
 
 export default Svg;
 Svg.Svg = Svg; // stay consistent with the old require based api
 
-export const Circle = createElement("Circle", "circle");
+export const Circle = createElement('Circle', 'circle');
 Svg.Circle = Circle;
 
-export const ClipPath = createElement("ClipPath", "clipPath");
+export const ClipPath = createElement('ClipPath', 'clipPath');
 Svg.ClipPath = ClipPath;
 
-export const Defs = createElement("Defs", "defs");
+export const Defs = createElement('Defs', 'defs');
 Svg.Defs = Defs;
 
-export const Ellipse = createElement("Ellipse", "ellipse");
+export const Ellipse = createElement('Ellipse', 'ellipse');
 Svg.Ellipse = Ellipse;
 
-export const ForeignObject = createElement("ForeignObject", "foreignObject");
+export const ForeignObject = createElement('ForeignObject', 'foreignObject');
 Svg.ForeignObject = ForeignObject;
 
-export const G = createElement("G", "g");
+export const G = createElement('G', 'g');
 Svg.G = G;
 
-export const Image = createElement("Image", "image");
+export const Image = createElement('Image', 'image');
 Svg.Image = Image;
 
-export const Line = createElement("Line", "line");
+export const Line = createElement('Line', 'line');
 Svg.Line = Line;
 
-export const LinearGradient = createElement("LinearGradient", "linearGradient");
+export const LinearGradient = createElement('LinearGradient', 'linearGradient');
 Svg.LinearGradient = LinearGradient;
 
-export const Marker = createElement("Marker", "marker");
+export const Marker = createElement('Marker', 'marker');
 Svg.Marker = Marker;
 
-export const Mask = createElement("Mask", "mask");
+export const Mask = createElement('Mask', 'mask');
 Svg.Mask = Mask;
 
-export const Path = createElement("Path", "path");
+export const Path = createElement('Path', 'path');
 Svg.Path = Path;
 
-export const Pattern = createElement("Pattern", "pattern");
+export const Pattern = createElement('Pattern', 'pattern');
 Svg.Pattern = Pattern;
 
-export const Polygon = createElement("Polygon", "polygon");
+export const Polygon = createElement('Polygon', 'polygon');
 Svg.Polygon = Polygon;
 
-export const Polyline = createElement("Polyline", "polyline");
+export const Polyline = createElement('Polyline', 'polyline');
 Svg.Polyline = Polyline;
 
-export const RadialGradient = createElement("RadialGradient", "radialGradient");
+export const RadialGradient = createElement('RadialGradient', 'radialGradient');
 Svg.RadialGradient = RadialGradient;
 
-export const Rect = createElement("Rect", "rect");
+export const Rect = createElement('Rect', 'rect');
 Svg.Rect = Rect;
 
-export const Stop = createElement("Stop", "stop");
+export const Stop = createElement('Stop', 'stop');
 Svg.Stop = Stop;
 
-export const Symbol = createElement("Symbol", "symbol");
+export const Symbol = createElement('Symbol', 'symbol');
 Svg.Symbol = Symbol;
 
-export const Text = createElement("Text", "text");
+export const Text = createElement('Text', 'text');
 Svg.Text = Text;
 
-export const TextPath = createElement("TextPath", "textPath");
+export const TextPath = createElement('TextPath', 'textPath');
 Svg.TextPath = TextPath;
 
-export const TSpan = createElement("TSpan", "tspan");
+export const TSpan = createElement('TSpan', 'tspan');
 Svg.TSpan = TSpan;
 
-export const Use = createElement("Use", "use");
+export const Use = createElement('Use', 'use');
 Svg.Use = Use;
 
 export class SvgXml extends React.Component {
+  handleXMLFill() {
+    if (this.props.fill) {
+      const removeFillRegex = /fill=("|')(?:[^\1\\]|\\.)+?\1/gi;
+      return (xmlWithFillRemoved = this.props.xml.replace(removeFillRegex, ''));
+    } else {
+      return this.props.xml;
+    }
+  }
 
   render() {
-    const removeFillRegex = /fill=("|')(?:[^\1\\]|\\.)+?\1/gi;
-    const xmlWithFillRemoved = this.props.xml.replace(removeFillRegex, '');
-
     const divProps = Object.assign({}, this.props, {
-      dangerouslySetInnerHTML: { __html: xmlWithFillRemoved },
+      dangerouslySetInnerHTML: { __html: this.handleXMLFill() },
       style: {
-        fill: this.props.fill,
-        height: this.props.height ? this.props.height + "px" : undefined,
-        width: this.props.width ? this.props.width + "px" : undefined,
+        fill: this.props.fill ? this.props.fill : undefined,
+        height: this.props.height ? this.props.height + 'px' : undefined,
+        width: this.props.width ? this.props.width + 'px' : undefined
       }
-    })
-    return createReactElement("div", divProps, this.props.children);
+    });
+    return createReactElement('div', divProps, this.props.children);
   }
 }
 SvgXml.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 SvgXml.defaultProps = {
-  children: undefined,
+  children: undefined
 };
 Svg.SvgXml = SvgXml;
